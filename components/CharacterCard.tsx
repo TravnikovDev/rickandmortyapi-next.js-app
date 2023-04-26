98fbc71ecca0cce11c8cf8bd20a3248ad6393004
Roman Travnikov <roman@travnikov.dev>
2023-04-26 23:47:03 +0300

    Styling components

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -1,4 +1,17 @@
-import { Box, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Text,
+  VStack,
+  Tag,
+  HStack,
+  Icon,
+  useColorModeValue,
+  useTheme,
+} from "@chakra-ui/react";
+import { BiWorld, BiPlanet } from "react-icons/bi";
+import { FaRegUserCircle } from "react-icons/fa";
+import { MdLocationOn } from "react-icons/md";
 import Link from "next/link";
 import { Character } from "../generated/graphql";
 
@@ -7,6 +20,14 @@ interface CharacterCardProps {
 }
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
+  const theme = useTheme();
+  const textColor = useColorModeValue(theme.colors.brand.text, "white");
+
+  const statusColor =
+    character.status === "Alive"
+      ? theme.colors.brand.alive
+      : theme.colors.brand.dead;
+
   return (
     <Link href={`/characters/${character.id}`}>
       <Box
@@ -16,15 +37,44 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ character }) => {
         cursor="pointer"
         _hover={{ boxShadow: "md" }}
         w="300px"
-        background={"rgba(255,255,255,0.75)"}
+        background={theme.colors.brand.background}
       >
         <Image src={`${character.image}`} alt={`${character.name}`} />
-        <VStack p={4} spacing={2}>
-          <Text fontSize="2xl" fontWeight="bold">
-            {character.name}
-          </Text>
-          <Text fontSize="lg">Species: {character.species}</Text>
-          <Text fontSize="lg">Status: {character.status}</Text>
+        <VStack p={4} spacing={2} alignItems="start">
+          <HStack spacing={2}>
+            <Text fontSize="2xl" fontWeight="bold" color={textColor}>
+              {character.name}
+            </Text>
+            <Tag size="sm" colorScheme="gray" borderRadius="full">
+              {character.status}
+            </Tag>
+            <Box bgColor={statusColor} borderRadius="50%" w="4px" h="4px" />
+          </HStack>
+          <HStack spacing={2}>
+            <Icon as={FaRegUserCircle} color={textColor} />
+            <Text fontSize="lg" color={textColor}>
+              Species: {character.species}
+            </Text>
+          </HStack>
+
+          <HStack spacing={2}>
+            <Icon as={BiWorld} color={textColor} />
+            <Text fontSize="lg" color={textColor}>
+              Origin: {character.origin?.name}
+            </Text>
+          </HStack>
+          <HStack spacing={2}>
+            <Icon as={MdLocationOn} color={textColor} />
+            <Text fontSize="lg" color={textColor}>
+              Location: {character.location?.name || "Unknown"}
+            </Text>
+          </HStack>
+          <HStack spacing={2}>
+            <Icon as={BiPlanet} color={textColor} />
+            <Text fontSize="lg" color={textColor}>
+              Type: {character.type || "Unknown"}
+            </Text>
+          </HStack>
         </VStack>
       </Box>
     </Link>
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,7 +1,108 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from "@chakra-ui/react";
+
+// Background: #202329 (dark gray)
+// Primary: #23CE6B (green, like the portal)
+// Secondary: #5A4FCF (blue, like Rick's portal gun)
+// Accent: #FF9800 (orange, for contrast)
+// Text: #FFFFFF (white)
 
 const theme = extendTheme({
-  // Your custom theme configuration goes here
+  colors: {
+    brand: {
+      background: "#202329",
+      primary: "#23CE6B",
+      secondary: "#5A4FCF",
+      accent: "#FF9800",
+      text: "#FFFFFF",
+      alive: "#2ecc71",
+      dead: "#e74c3c",
+    },
+  },
+  components: {
+    Heading: {
+      baseStyle: {
+        color: "brand.text",
+      },
+    },
+    Box: {
+      baseStyle: {
+        color: "brand.text",
+      },
+    },
+    Container: {
+      baseStyle: {
+        maxW: "container.xl",
+      },
+    },
+    SimpleGrid: {
+      baseStyle: {
+        color: "brand.text",
+      },
+    },
+    Flex: {
+      baseStyle: {
+        color: "brand.text",
+      },
+    },
+    Spinner: {
+      baseStyle: {
+        color: "brand.secondary",
+      },
+    },
+    Text: {
+      baseStyle: {
+        color: "brand.text",
+      },
+    },
+    Input: {
+      baseStyle: {
+        borderColor: "brand.secondary",
+        _hover: {
+          borderColor: "brand.primary",
+        },
+        _focus: {
+          borderColor: "brand.primary",
+        },
+      },
+    },
+    Select: {
+      baseStyle: {
+        borderColor: "brand.secondary",
+        bg: "brand.background",
+        color: "brand.text",
+        _hover: {
+          borderColor: "brand.primary",
+        },
+        _focus: {
+          borderColor: "brand.primary",
+          boxShadow: "0 0 0 1px #7CD7D9",
+        },
+      },
+    },
+    Switch: {
+      baseStyle: {
+        track: {
+          _checked: {
+            bg: "brand.primary",
+          },
+        },
+        thumb: {
+          _checked: {
+            bg: "brand.secondary",
+          },
+        },
+      },
+    },
+    Button: {
+      baseStyle: {
+        bg: "brand.primary",
+        color: "brand.text",
+        _hover: {
+          bg: "brand.secondary",
+        },
+      },
+    },
+  },
 });
 
 export default theme;
