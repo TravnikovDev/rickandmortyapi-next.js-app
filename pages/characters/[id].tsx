171566f615eac0a93da2463000314fafde0bac1f
Roman Travnikov <roman@travnikov.dev>
2023-04-27 17:07:49 +0300

    Update pagination and character page styles. Fixed search and pagination logic

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -8,10 +8,16 @@ import {
   FormLabel,
   Input,
   Image,
+  Text,
+  VStack,
+  Spinner,
+  HStack,
+  IconButton,
 } from "@chakra-ui/react";
 import { useGetCharacterByIdQuery } from "../../generated/graphql";
 import Layout from "../../components/Layout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const CharacterPage: NextPage = () => {
   const router = useRouter();
@@ -27,6 +33,10 @@ const CharacterPage: NextPage = () => {
   const [editing, setEditing] = useState(false);
   const [editedName, setEditedName] = useState(character?.name || "");
 
+  useEffect(() => {
+    if (character?.name) setEditedName(character?.name);
+  }, [character?.name]);
+
   const handleEditClick = () => {
     setEditing(true);
   };
@@ -39,39 +49,112 @@ const CharacterPage: NextPage = () => {
     setEditedName(event.target.value);
   };
 
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
     <Layout title={editedName}>
-      {loading && <p>Loading...</p>}
-      {error && <p>Error fetching character</p>}
+      <Box textAlign="center" marginTop="4">
+        <HStack mb={4} spacing={4} w={'auto'}>
+          <IconButton
+            aria-label="Go back"
+            icon={<ArrowBackIcon />}
+            onClick={handleBackClick}
+            colorScheme="brand"
+            background="brand.primary"
+            color="brand.text"
+            _hover={{
+              background: "brand.primary",
+              opacity: 0.8,
+            }}
+            _active={{
+              background: "brand.primary",
+              opacity: 0.6,
+            }}
+          />
+          <Text fontSize="2xl" fontWeight="bold">
+            Back to list
+          </Text>
+        </HStack>
+        {loading && (
+          <Spinner size="xl" color="brand.primary" thickness="6px" speed="2s" />
+        )}
+        {error && <p>Error fetching character</p>}
+      </Box>
       {character && (
-        <Flex direction="column" alignItems="center">
-          <Image src={`${character.image}`} alt={`${character.name}`} />
-          {editing ? (
-            <FormControl mt={4}>
-              <FormLabel htmlFor="name">Name</FormLabel>
-              <Input
-                type="text"
-                id="name"
-                value={editedName}
-                onChange={handleNameChange}
-              />
-            </FormControl>
-          ) : (
-            <h2>{editedName}</h2>
-          )}
-          <p>Species: {character.species}</p>
-          <p>Status: {character.status}</p>
-          <p>Origin: {character?.origin?.name}</p>
-          {!editing ? (
-            <Button onClick={handleEditClick} mt={4}>
-              Edit
-            </Button>
-          ) : (
-            <Button onClick={handleSaveClick} mt={4}>
-              Save
-            </Button>
-          )}
-        </Flex>
+        <Box
+          maxWidth="md"
+          mx="auto"
+          borderWidth={2}
+          borderRadius="lg"
+          borderColor="brand.primary"
+          backgroundColor="brand.background"
+          p={4}
+          mt={4}
+        >
+          <VStack spacing={4} alignItems="center">
+            <Image
+              src={`${character.image}`}
+              alt={`${character.name}`}
+              borderRadius="full"
+              borderWidth={4}
+              borderColor="brand.primary"
+              boxSize="200px"
+            />
+            {editing ? (
+              <FormControl>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <Input
+                  type="text"
+                  id="name"
+                  value={editedName}
+                  onChange={handleNameChange}
+                  background="brand.background"
+                  borderColor="transparent"
+                  borderRadius="none"
+                  _hover={{
+                    borderColor: "transparent",
+                  }}
+                  _focus={{
+                    borderColor: "brand.primary",
+                    boxShadow: "none",
+                  }}
+                  color="brand.text"
+                  fontWeight="medium"
+                />
+              </FormControl>
+            ) : (
+              <Text fontSize="2xl" fontWeight="bold" color="brand.text">
+                {editedName}
+              </Text>
+            )}
+            <Text fontSize="lg" color="brand.text">
+              Species: {character.species}
+            </Text>
+            <Text fontSize="lg" color="brand.text">
+              Status: {character.status}
+            </Text>
+            <Text fontSize="lg" color="brand.text">
+              Origin: {character?.origin?.name}
+            </Text>
+            {!editing ? (
+              <Button
+                onClick={handleEditClick}
+                colorScheme="brand.primaryScheme"
+              >
+                Edit
+              </Button>
+            ) : (
+              <Button
+                onClick={handleSaveClick}
+                colorScheme="brand.primaryScheme"
+              >
+                Save
+              </Button>
+            )}
+          </VStack>
+        </Box>
       )}
     </Layout>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,20 +32,20 @@ const HomePage: NextPage = () => {
   // Handling content render without affecting rest of layout
   if (loading) {
     content = (
-      <Box textAlign="center" marginTop="4">
-        <Spinner />
+      <Box textAlign="center" fontSize={48} fontWeight={"bold"} mt={56}>
+        <Spinner size="xl" color="brand.primary" thickness="6px" speed="2s" />
       </Box>
     );
   } else if (error || !characterList) {
     content = (
-      <Box textAlign="center" marginTop="4">
+      <Box textAlign="center" fontSize={48} fontWeight={"bold"} mt={56}>
         <Text>Error fetching characters.</Text>
       </Box>
     );
   } else if (characterList.length === 0) {
     content = (
-      <Box textAlign="center" marginTop="4">
-        <Text>Error fetching characters.</Text>
+      <Box textAlign="center" fontSize={48} fontWeight={"bold"} mt={56}>
+        <Text>No characters found matching your search criteria.</Text>
       </Box>
     );
   } else {
@@ -58,8 +58,6 @@ const HomePage: NextPage = () => {
     );
   }
 
-  console.log(characterList);
-
   return (
     <Layout title="The Rick and Morty characters">
       <Box margin="0 auto" marginTop="4" maxW={"xl"}>
@@ -68,11 +66,13 @@ const HomePage: NextPage = () => {
       </Box>
       {content}
       <Flex my={8} justifyContent="center" width="100%">
-        <Pagination
-          currentPage={pageNumber}
-          totalPages={info?.pages || 1}
-          onPageChange={handlePageChange}
-        />
+        {info && characterList.length > 0 && (
+          <Pagination
+            currentPage={pageNumber}
+            totalPages={info?.pages || 1}
+            onPageChange={handlePageChange}
+          />
+        )}
       </Flex>
     </Layout>
   );
